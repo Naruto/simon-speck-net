02acf7c0827243cac35d5ccb8ce7cc5b30fc6bbf
Naruto TAKAHASHI <naruto.takahashi@dena.com>
2017-07-10 20:24:02 +0900

    add solution file

---
+++ speck/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace System.Security.Cryptography.Extention
+{
+    public class Speck : System.Security.Cryptography.SymmetricAlgorithm
+    {
+        public override ICryptoTransform CreateDecryptor(byte[] rgbKey, byte[] rgbIV)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ICryptoTransform CreateEncryptor(byte[] rgbKey, byte[] rgbIV)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GenerateIV()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void GenerateKey()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
