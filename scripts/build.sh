cb4fc1724d3fc8b43bbc969c4a5a00fa3b40a434
Naruto TAKAHASHI <tnaruto@gmail.com>
2017-09-17 02:36:17 +0900

    release build

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,6 @@ BASEDIR=`cd ${BASEDIR} && pwd -P`
 
 pushd ${BASEDIR}/net > /dev/null
 mono .nuget/nuget.exe restore
-xbuild /p:TargetFrameworkVersion="v4.5"
-mono ./packages/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe ./speckTest/bin/Debug/speckTest.dll
+xbuild /p:TargetFrameworkVersion="v4.5" /p:Configuration=Release
+mono ./packages/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe ./speckTest/bin/Release/speckTest.dll
 popd > /dev/null # ${BASEDIR}
