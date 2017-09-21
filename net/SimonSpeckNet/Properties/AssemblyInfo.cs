326f2bac7769a1fd429c4ef96db3c5af3e384563
Naruto TAKAHASHI <tnaruto@gmail.com>
2017-09-22 00:16:42 +0900

    update SimonSpeckNet infomation

--- net/SimonSpeckNet/Properties/AssemblyInfo.cs
+++ net/SimonSpeckNet/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("speck")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SimonSpeckNet")]
+[assembly: AssemblyDescription("Speck Simon are lightweight block ciphers: those block ciphers are efficiently and provide high performance across a range of devices.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("speck")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Simon Speck C#")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Naruto TAKAHASHI")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
